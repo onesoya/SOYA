3cdaa22f22d9b131fe4462a37d9ab5102e5fcfaa
onesoya <sjsj980415@gmail.com>
2026-09-02 14:36:37 +0900

    Restrict Firebase deploy identity permissions

--- .github/setup-google-wif.sh
+++ .github/setup-google-wif.sh
@@ -8,6 +8,7 @@ soya_pool_id="github-actions"
 soya_provider_id="onesoya-soya"
 soya_service_account_id="github-actions-firebase"
 soya_service_account_email="${soya_service_account_id}@${soya_project_id}.iam.gserviceaccount.com"
+soya_workflow_ref="${soya_repository}/.github/workflows/deploy-firebase-functions.yml@refs/heads/main"
 
 gcloud config set project "${soya_project_id}"
 gcloud services enable \
@@ -22,7 +23,6 @@ fi
 
 for soya_role in \
   roles/cloudfunctions.admin \
-  roles/iam.serviceAccountUser \
   roles/serviceusage.serviceUsageConsumer \
   roles/firebase.viewer
 do
@@ -30,6 +30,18 @@ do
     --member="serviceAccount:${soya_service_account_email}" \
     --role="${soya_role}" \
     --condition=None >/dev/null
+done
+
+for soya_runtime_service_account in \
+  "${soya_project_number}-compute@developer.gserviceaccount.com" \
+  "${soya_project_id}@appspot.gserviceaccount.com"
+do
+  if gcloud iam service-accounts describe "${soya_runtime_service_account}" >/dev/null 2>&1; then
+    gcloud iam service-accounts add-iam-policy-binding "${soya_runtime_service_account}" \
+      --project="${soya_project_id}" \
+      --member="serviceAccount:${soya_service_account_email}" \
+      --role="roles/iam.serviceAccountUser" >/dev/null
+  fi
 done
 
 if ! gcloud iam workload-identity-pools describe "${soya_pool_id}" \
@@ -53,8 +65,8 @@ then
     --workload-identity-pool="${soya_pool_id}" \
     --display-name="onesoya/SOYA main" \
     --issuer-uri="https://token.actions.githubusercontent.com" \
-    --attribute-mapping="google.subject=assertion.sub,attribute.repository=assertion.repository,attribute.ref=assertion.ref" \
-    --attribute-condition="assertion.repository=='${soya_repository}' && assertion.ref=='refs/heads/main'"
+    --attribute-mapping="google.subject=assertion.sub,attribute.repository=assertion.repository,attribute.ref=assertion.ref,attribute.workflow=assertion.job_workflow_ref" \
+    --attribute-condition="assertion.repository=='${soya_repository}' && assertion.ref=='refs/heads/main' && assertion.job_workflow_ref=='${soya_workflow_ref}'"
 fi
 
 gcloud iam service-accounts add-iam-policy-binding "${soya_service_account_email}" \
